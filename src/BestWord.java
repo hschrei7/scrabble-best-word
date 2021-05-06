122626c57a12cf83df993f5086ccb8ccc35b20a2
aoch1019 <andrewoch95@gmail.com>
2021-05-06 15:42:08 -0400

    Finished valid placements method

--- src/BestWord.java
+++ src/BestWord.java
@@ -108,6 +108,7 @@ public class BestWord implements IBestWord {
     public Entry<ArrayList<Entry<Integer, Entry<Integer, Integer>>>, ArrayList<Entry<Integer, Entry<Integer, Integer>>>> allValidTilePlacements(ArrayList<Entry<Integer, Integer>> anchors){
         
         ArrayList<Entry<Integer, Entry<Integer, Integer>>> verticalPlacements = new ArrayList<Entry<Integer, Entry<Integer, Integer>>>();
+        ArrayList<Entry<Integer, Entry<Integer, Integer>>> horizontalPlacements = new ArrayList<Entry<Integer, Entry<Integer, Integer>>>();
         
         for(Entry<Integer, Integer> anchor : anchors) {
             //try all vertical combos above and below anchor
@@ -124,24 +125,44 @@ public class BestWord implements IBestWord {
                     //We have identified a valid vertical range to add tiles. Add that range to our answer. 
                     Entry<Integer, Integer> rowRange = new SimpleEntry<Integer, Integer>(above, below);
                     Entry<Integer, Entry<Integer, Integer>> colAndRange = new SimpleEntry<Integer, Entry<Integer, Integer>>(anchor.getValue(), rowRange);
-                    
-                    System.out.println("The anchor Row: " + anchor.getKey() + " Col: " + anchor.getValue());
-                    System.out.println("has range " + rowRange.getKey() + " - "+ rowRange.getValue());
+                    verticalPlacements.add(colAndRange);
+//                    System.out.println("The anchor Row: " + anchor.getKey() + " Col: " + anchor.getValue());
+//                    System.out.println("has range " + rowRange.getKey() + " - "+ rowRange.getValue());
                 }
             }
             //try all horizontal combos to left and right of anchor
+            for(int i = 0; i < 7; i++) {
+                int left = collectPlacementsLeft(anchor, i);
+                if(left == -1) {
+                    continue;
+                }
+                for(int j = 6 - i; j >= 0; j--) {
+                    int right = collectPlacementsRight(anchor, j);
+                    if(right == -1) {
+                        continue;
+                    }
+                    //We have identified a valid horizontal range to add tiles. Add that range to our answer. 
+                    Entry<Integer, Integer> colRange = new SimpleEntry<Integer, Integer>(left, right);
+                    Entry<Integer, Entry<Integer, Integer>> rowAndRange = new SimpleEntry<Integer, Entry<Integer, Integer>>(anchor.getKey(), colRange);
+                    horizontalPlacements.add(rowAndRange);
+                    System.out.println("The anchor Row: " + anchor.getKey() + " Col: " + anchor.getValue());
+                    System.out.println("has horizontal range " + colRange.getKey() + " - "+ colRange.getValue());
+                }
+            }
         }
-        return null;
+        
+        Entry<ArrayList<Entry<Integer, Entry<Integer, Integer>>>, ArrayList<Entry<Integer, Entry<Integer, Integer>>>> placements = new SimpleEntry<ArrayList<Entry<Integer, Entry<Integer, Integer>>>, ArrayList<Entry<Integer, Entry<Integer, Integer>>>>(verticalPlacements, horizontalPlacements);
+        return placements;
     }
     
-    private int collectPlacementsAbove(Entry<Integer, Integer> anchor, int range){
-        
-        if(range == 0) {
-            return 0;
-        }        
+    private int collectPlacementsAbove(Entry<Integer, Integer> anchor, int range){    
         
         int row = anchor.getKey();
         int col = anchor.getValue();
+        
+        if(range == 0) {
+            return row;
+        }      
         
         for(int i = row-1; i >= 0; i--) {
             if(this.intBoard[i][col] == 0) {
@@ -155,13 +176,13 @@ public class BestWord implements IBestWord {
     }
     
     private int collectPlacementsBelow(Entry<Integer, Integer> anchor, int range){
-
-        if(range == 0) {
-            return 0;
-        }
         
         int row = anchor.getKey();
         int col = anchor.getValue();
+        
+        if(range == 0) {
+            return row;
+        }        
         
         for(int i = row+1; i <= 14; i++) {
             if(this.intBoard[i][col] == 0) {
@@ -172,7 +193,47 @@ public class BestWord implements IBestWord {
             }
         }
         return -1;
-    }    
+    }
+    
+    private int collectPlacementsLeft(Entry<Integer, Integer> anchor, int range){    
+        
+        int row = anchor.getKey();
+        int col = anchor.getValue();
+        
+        if(range == 0) {
+            return col;
+        }      
+        
+        for(int i = col-1; i >= 0; i--) {
+            if(this.intBoard[row][i] == 0) {
+                range--;
+                if(range == 0) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+    
+    private int collectPlacementsRight(Entry<Integer, Integer> anchor, int range){    
+        
+        int row = anchor.getKey();
+        int col = anchor.getValue();
+        
+        if(range == 0) {
+            return col;
+        }      
+        
+        for(int i = col+1; i <= 14; i++) {
+            if(this.intBoard[row][i] == 0) {
+                range--;
+                if(range == 0) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }      
     
     /**
      * Iterates through the list of placements and finds all valid words for each 
