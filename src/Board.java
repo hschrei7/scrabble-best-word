a966aa32a3980f87161c010867a8c4ecdc2b0ece
Joey Santillo <jsantillo23@gmail.com>
2021-05-07 14:53:34 -0400

    Committing Henry's changes

--- src/Board.java
+++ src/Board.java
@@ -5,76 +5,84 @@ import java.util.Scanner;
 
 public class Board implements IBoard {
 
-	Square[][] board;
-	Bag bag;
-	Tile[] rack;
+    Square[][] board;
+    Bag bag;
+    Tile[] rack;
 
     Board(){
-		initializeSquares();
-		this.bag = new Bag();
-	}
-	
-	/**
-	 * Initializes the board with the appropriate squares.
-	 */
-	public void initializeSquares() {
-		int[][] nums = {{4, 0, 0, 1, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 4},
-						{0, 3, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 3, 0},
-						{0, 0, 3, 0, 0, 0, 1, 0, 1, 0, 0, 0, 3, 0, 0},
-						{1, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 0},
-						{0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0},
-						{0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0},
-						{0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0},
-						{4, 0, 0, 1, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0, 4},
-						{0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0},
-						{0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0},
-						{0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0},
-						{1, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 0},
-						{0, 0, 3, 0, 0, 0, 1, 0, 1, 0, 0, 0, 3, 0, 0},
-						{0, 3, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 3, 0},
-						{4, 0, 0, 1, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 4}};
-		this.board = new Square[15][15];
-		for(int i = 0; i < nums.length; i++) {
-			for(int j = 0; j < nums[i].length; j++) {
-				this.board[i][j] = new Square(nums[i][j]);
-			}
-		}
-	}
-	
-	public Square[][] getBoard() {
-		return board;
-	}
+        initializeSquares();
+        this.bag = new Bag();
+    }
+    
+    /**
+     * Initializes the board with the appropriate squares.
+     */
+    public void initializeSquares() {
+        int[][] nums = {{4, 0, 0, 1, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 4},
+                        {0, 3, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 3, 0},
+                        {0, 0, 3, 0, 0, 0, 1, 0, 1, 0, 0, 0, 3, 0, 0},
+                        {1, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 0},
+                        {0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0},
+                        {0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0},
+                        {0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0},
+                        {4, 0, 0, 1, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0, 4},
+                        {0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0},
+                        {0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0},
+                        {0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0},
+                        {1, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 0},
+                        {0, 0, 3, 0, 0, 0, 1, 0, 1, 0, 0, 0, 3, 0, 0},
+                        {0, 3, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 3, 0},
+                        {4, 0, 0, 1, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 4}};
+        this.board = new Square[15][15];
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = 0; j < nums[i].length; j++) {
+                this.board[i][j] = new Square(nums[i][j]);
+            }
+        }
+    }
+    
+    public Square[][] getBoard() {
+        return board;
+    }
 
-	public void setBoard(Square[][] board) {
-		this.board = board;
-	}
-	
-	public Bag getBag() {
-	    return this.bag;
-	}
-	
+    public void setBoard(Square[][] board) {
+        this.board = board;
+    }
+    
+    public Bag getBag() {
+        return this.bag;
+    }
+    
     public Tile[] getRack() {
         return rack;
     }
 
     public void setRack(Tile[] rack) {
         this.rack = rack;
-    }	
+    }   
 
-	public void printBoard() {
-		for(Square[] sqArr : board) {
-			for(Square sq : sqArr) {
-				System.out.print(sq.getSquareString() + " | ");
-			}
-			System.out.println("");
-			System.out.println("--------------------------------------------------------------------------");
-		}
-	}
-	
-	public void placeTile(Tile t, int row, int col) {
-		board[row][col].setTile(t);
-	}
-	
+    public void printBoard() {
+        for(Square[] sqArr : board) {
+            for(Square sq : sqArr) {
+                System.out.print(sq.getSquareString() + " | ");
+            }
+            System.out.println("");
+            System.out.println("--------------------------------------------------------------------------");
+        }
+    }
+    
+    public void placeTile(Tile t, int row, int col) {
+        board[row][col].setTile(t);
+    }
+    
+    public Tile getTile(int row, int col) {
+        return board[row][col].getTile();
+    }
+    
+    public Square getSquare(int row, int col) {
+        return board[row][col];
+    }
+    
     /**
      * Creates a random board
      */
@@ -136,12 +144,12 @@ public class Board implements IBoard {
             e.printStackTrace();
         }
     }
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Board b = new Board();		
-		b.printBoard();
-		b.createRandomBoard();
-		b.printBoard();
-	}
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Board b = new Board();      
+        b.printBoard();
+        b.createRandomBoard();
+        b.printBoard();
+    }
 }
